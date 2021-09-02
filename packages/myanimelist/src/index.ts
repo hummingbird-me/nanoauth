45881b41ef972954f05779fc4d9dbfbafc1f03c3
Emma Lejeck <nuck@kitsu.io>
2021-09-01 20:55:33 -0700

    Use middleware for MAL client

--- packages/myanimelist/src/index.ts
+++ packages/myanimelist/src/index.ts
@@ -1,4 +1,5 @@
-import { generateRandom64, getAuthorization } from '@nanoauth/base';
+import { getAuthorization, OAuth2Response } from '@nanoauth/base';
+import { pkce, state } from '@nanoauth/middleware';
 
 export default async function getMyAnimeListAuthorization({
   clientId,
@@ -6,21 +7,17 @@ export default async function getMyAnimeListAuthorization({
 }: {
   clientId: string;
   redirectUri: string;
-}): Promise<Record<string, unknown>> {
-  const pkce = generateRandom64(32);
-  const state = generateRandom64(32);
+}): Promise<OAuth2Response> {
   const params = new URLSearchParams({
     response_type: 'code',
     redirect_uri: redirectUri,
     client_id: clientId,
-    code_challenge: pkce,
-    code_challenge_method: 'plain',
-    state,
   });
   const url = new URL('https://myanimelist.net/v1/oauth2/authorize');
   url.search = params.toString();
-  console.log(url.toString());
-  const result = await getAuthorization(url);
 
-  return {};
+  return getAuthorization({
+    url: url.toString(),
+    middlewares: [pkce(), state()],
+  });
 }
