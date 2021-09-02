283b8cb37774f44389c658389ab31cdc763faa4f
Emma Lejeck <nuck@kitsu.io>
2021-09-01 21:09:16 -0700

    Tweak tsup configs

--- packages/base/tsup.config.ts
+++ packages/base/tsup.config.ts
@@ -2,10 +2,9 @@ import type { Options } from 'tsup';
 
 export const tsup: Options = {
   splitting: false,
-  target: 'esnext',
   sourcemap: true,
   clean: true,
-  entryPoints: ['src/index.ts', 'src/handleRedirect.ts'],
+  entryPoints: ['src/index.ts'],
   format: ['cjs', 'esm'],
   dts: true,
   minify: true,
