ba21f496c4ed3063bb2462046cb8ec22182315e4
Emma Lejeck <nuck@kitsu.io>
2021-09-02 23:13:55 -0700

    Add tests for PKCE middleware and prepare for PKCE S256 support

---
+++ packages/middleware/src/pkce.test.ts
@@ -0,0 +1,35 @@
+import pkcePlain from './pkce';
+
+jest.mock('./util/random64');
+
+describe('Plain PKCE middleware', () => {
+  it('should add PKCE parameters to the URL without removing other params', async () => {
+    const url = 'https://example.com/oauth2.html?foo=bar';
+    const middleware = pkcePlain();
+
+    await middleware(url, async (url) => {
+      const parsedUrl = new URL(url);
+      const params = new URLSearchParams(parsedUrl.search);
+      expect(params.has('code_challenge')).toBeTruthy();
+      expect(params.get('code_challenge_method')).toBe('plain');
+      return {};
+    });
+  });
+
+  it('should return the generated code verifier in the parameters', async () => {
+    const url = 'https://example.com/oauth2.html';
+    const middleware = pkcePlain();
+
+    let codeChallenge = '';
+
+    const result = await middleware(url, async (url) => {
+      const parsedUrl = new URL(url);
+      const params = new URLSearchParams(parsedUrl.search);
+      codeChallenge = params.get('code_challenge') as string;
+      expect(params.get('code_challenge_method')).toBe('plain');
+      return {};
+    });
+
+    expect(codeChallenge).toEqual(result.codeVerifier);
+  });
+});
--- packages/middleware/src/pkce.ts
+++ packages/middleware/src/pkce.ts
@@ -2,16 +2,17 @@ import { Middleware } from '@nanoauth/base';
 
 import generateRandom64 from './util/random64';
 
-const pkceMiddleware: Middleware = () => async (_url, next) => {
-  const pkce = generateRandom64(32);
+const pkcePlainMiddleware: Middleware = () => async (_url, next) => {
+  const codeVerifier = generateRandom64(32);
   const url = new URL(_url);
   const params = new URLSearchParams(url.search);
-  params.set('code_challenge', pkce);
+  params.set('code_challenge', codeVerifier);
   params.set('code_challenge_method', 'plain');
   url.search = params.toString();
 
   const result = await next(url.toString());
 
-  return { ...result, pkce };
+  return { ...result, codeVerifier };
 };
-export default pkceMiddleware;
+
+export default pkcePlainMiddleware;
