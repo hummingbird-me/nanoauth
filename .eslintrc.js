e79ae3560c204235b76aec8a88affdb916aede57
Emma Lejeck <nuck@kitsu.io>
2021-09-02 21:05:16 -0700

    Configure eslint for config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,22 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   env: {
-    browser: true
-  }
-}
+    browser: true,
+  },
+  overrides: [
+    {
+      files: ['.eslintrc.js', '*.config.{js,ts}'],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015,
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+    },
+  ],
+};
